33c0908aaf001e809c12800d6ba2f23afb602ac0
Satyam Abhishek <satiitmadras123@gmail.com>
2021-03-06 14:18:33 +0530

    set up routers and controllers

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+console.log('router loaded');
+
+const homeController= require('../controllers/homeController');
+router.get('/',homeController.home);
+
+module.exports = router;
