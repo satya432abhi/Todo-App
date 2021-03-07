f5bc2a28112bbc526e1db614a2c756e9afd1c095
Satyam Abhishek <satiitmadras123@gmail.com>
2021-03-07 23:10:49 +0530

    database connected and information fetched from db

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,20 @@
+const Todo = require("../models/Todo");
+
 module.exports.home = function(req,res){
-    res.end('<h1>Todo App is ready to be built</h1>');
-}
+    // res.end('<h1>Todo App is ready to be built</h1>');
+
+    Todo.find({},function(err,todo_list)
+    {
+        
+        if(err)
+        {
+            console.log('error in fetching from the database');
+            return;
+        }
+       
+        return res.render('home',{
+          title: "TODO App",
+          list : todo_list,
+        });
+    });
+};
--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 const express = require('express');
 const app = express();
-const port = 8000;
 
+const db = require('./config/mongoose');
+const Todo = require('./models/Todo');
+
+const port = 8000;
 //use express router
 app.use('/',require('./routes/index'));
 
 //set ejs as the view-engine for express
 app.set('view engine','ejs');
 app.set('views','./views');
+
+
+/*used as middle-ware or as a parser*/
+app.use(express.urlencoded());
+app.use(express.static('assets'));
+//call karu ? ok 8980473102
+
+app.post('/Add-task',function(req,res){
+
+    Todo.create({
+        description:req.body.description,
+        category:req.body.category,
+        date:req.body.deadline,
+
+    },function(err,newTask){
+        if(err)
+        {
+            console.log("error in adding a task!",err);
+            return ;
+        }
+        console.log('********',newTask);
+        return res.redirect('back');
+    });
+    
+    console.log(req);
+    //  return res.redirect('/');
+    // return res.redirect('back');
+    console.log(req.body.description);
+    console.log(req.body.category);
+});
 
 app.listen(port,function(err){
     if(err)
